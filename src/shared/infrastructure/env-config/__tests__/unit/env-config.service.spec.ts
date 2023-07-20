346bde5d338092c07a9b6c056be188a4c9076012
danguya <daniel.yava16@gmail.com>
2023-07-20 12:32:56 +0100

    Adding users resource

--- src/shared/infrastructure/env-config/__tests__/unit/env-config.service.spec.ts
+++ src/shared/infrastructure/env-config/__tests__/unit/env-config.service.spec.ts
@@ -1,28 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { EnvConfigService } from '../../env-config.service';
-import { EnvConfigModule } from '../../env-config.module';
+import { Test, TestingModule } from '@nestjs/testing'
+import { EnvConfigService } from '../../env-config.service'
+import { EnvConfigModule } from '../../env-config.module'
 
 describe('EnvConfigService unit tests', () => {
-  let sut: EnvConfigService;
+  let sut: EnvConfigService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [EnvConfigModule.forRoot({})],
       providers: [EnvConfigService],
-    }).compile();
+    }).compile()
 
-    sut = module.get<EnvConfigService>(EnvConfigService);
-  });
+    sut = module.get<EnvConfigService>(EnvConfigService)
+  })
 
   it('should be defined', () => {
-    expect(sut).toBeDefined();
-  });
+    expect(sut).toBeDefined()
+  })
 
   it('should return the variable PORT', () => {
-    expect(sut.getAppPort()).toBe(3000);
-  });
+    expect(sut.getAppPort()).toBe(3000)
+  })
 
   it('should return the variable NODE_ENV', () => {
-    expect(sut.getNodeEnv()).toBe('test');
-  });
-});
+    expect(sut.getNodeEnv()).toBe('test')
+  })
+})
