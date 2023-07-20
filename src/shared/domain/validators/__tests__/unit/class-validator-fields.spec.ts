085944c08f869d39177c6e42bf6112cadf7fc794
danguya <daniel.yava16@gmail.com>
2023-07-20 18:54:22 +0100

    ClassValidatorFields - unit tests part 2

--- src/shared/domain/validators/__tests__/unit/class-validator-fields.spec.ts
+++ src/shared/domain/validators/__tests__/unit/class-validator-fields.spec.ts
@@ -23,4 +23,14 @@ describe('ClassValidatorFields unit tests', () => {
     expect(sut.validatedData).toBeNull()
     expect(sut.errors).toStrictEqual({ field: ['test error'] })
   })
+
+  it('should validate without errors', () => {
+    const spyValidateSync = jest.spyOn(libClassValidator, 'validateSync')
+    spyValidateSync.mockReturnValue([])
+    const sut = new StubClassValidatorFields()
+    expect(sut.validate({ field: 'value' })).toBeTruthy()
+    expect(spyValidateSync).toHaveBeenCalled()
+    expect(sut.validatedData).toStrictEqual({ field: 'value' })
+    expect(sut.errors).toBeNull()
+  })
 })
