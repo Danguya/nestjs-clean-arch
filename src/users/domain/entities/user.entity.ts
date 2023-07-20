2bc5216b2dd535ad489bf508c86b1322560bea20
danguya <daniel.yava16@gmail.com>
2023-07-20 14:22:58 +0100

    Adding Update methods

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -12,15 +12,28 @@ export class UserEntity extends Entity<UserProps> {
     super(props, id)
     this.props.createdAt = this.props.createdAt ?? new Date()
   }
-
+  update(value: string): void {
+    this.name = value
+  }
+  updatePassword(value: string): void {
+    this.password = value
+  }
   get name() {
     return this.props.name
   }
+  private set name(value: string) {
+    this.props.name = value
+  }
+
   get email() {
     return this.props.email
   }
+
   get password() {
     return this.props.password
+  }
+  private set password(value: string) {
+    this.props.password = value
   }
   get createdAt() {
     return this.props.createdAt
