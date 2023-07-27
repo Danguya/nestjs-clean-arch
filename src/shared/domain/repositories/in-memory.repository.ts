0eb50de7a82946d59067068b04ddc06e04afd0d3
danguya <daniel.yava16@gmail.com>
2023-07-27 11:28:20 +0100

    InMemoryRepository unit tests - delete method

--- src/shared/domain/repositories/in-memory.repository.ts
+++ src/shared/domain/repositories/in-memory.repository.ts
@@ -27,7 +27,7 @@ export abstract class InMemoryRepository<E extends Entity>
     await this._get(id)
 
     const index = await this._getIndex(id)
-    this.items.slice(index, 1)
+    this.items.splice(index, 1)
   }
 
   protected async _get(id: string): Promise<E> {
